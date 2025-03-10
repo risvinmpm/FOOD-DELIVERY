529dcac251eed027fb14771c4cebd4db2048c7b6
risvinmpm <risvinmp39@gmail.com>
2025-03-10 13:53:31 +0530

    explore menu completed

--- src/components/ExploreMenu/ExploreMenu.jsx
+++ src/components/ExploreMenu/ExploreMenu.jsx
@@ -2,22 +2,24 @@ import React from 'react'
 import './ExploreMenu.css'
 import { menu_list } from '../../assets/frontend_assets/assets'
 
-const ExploreMenu = () => {
+const ExploreMenu = ({category,setCategory}) => {
+
   return (
     <div className='explore-menu' id='explore-menu'>
-      <div className="explore-menu-text">
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas dolore aliquid cumque accusamus quis repellat, facere reiciendis adipisci expedita architecto modi reprehenderit earum ad quibusdam laboriosam eveniet velit cum sequi.</p>
+        <h1>Explore our menu</h1>
+      <p className="explore-menu-text">
+        Choose from a diverse menu featuring a delecatable array of dishes. Our mission is to satisfy your cravings and elevate your dining experience, one delicious meal at a time.</p>
         <div className='explore-menu-list'>
           {menu_list.map((item, index) => {
             return (
-              <div key={index} className='explore-menu-list-item'>
-                <img src={item.menu_image} alt="" />
+              <div onClick={()=>setCategory(prev=>prev===item.menu_name?"All":item.menu_name)} key={index} className='explore-menu-list-item'>
+                <img className={category===item.menu_name?"active":""} src={item.menu_image} alt="" />
                 <p>{item.menu_name}</p>
               </div>
             )
           })}
         </div>
-      </div>
+        <hr/>
     </div>
   )
 }
